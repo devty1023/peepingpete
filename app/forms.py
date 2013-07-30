a9407023824c3a955e40c5e727a33fa3884926be
devty <devty1023@gmail.com>
2013-07-30 11:45:48 -0400

    cleaning up

--- app/forms.py
+++
@@ -1,5 +0,0 @@
-from flask.ext.wtf import Form, TextField, SubmitField, validators, ValidationError
- 
-class SearchForm(Form):
-  course = TextField("Course", [validators.Required()])
-  submit = SubmitField("Submit")
--- app/test.py
+++
@@ -1,8 +0,0 @@
-
-import info_retrieve
-
-ret = info_retrieve.getCourse("cs252")
-if ret == None:
-  print "hey"
-
-print ret
